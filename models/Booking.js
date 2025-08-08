6f09c4537ebecc03a3708b6b7df3b13ca1185c34
Aditya Singh <83707197+Aditya-Singh714@users.noreply.github.com>
2025-08-08 13:05:48 +0530

    Booking and Quotation routes and models

--- models/Booking.js
+++ models/Booking.js
@@ -1,12 +1,22 @@
 import mongoose from "mongoose";
+
 const bookingSchema = new mongoose.Schema({
-  name: String,
-  mobile: String,
-  purpose: String,
-  slotDate: String,
-  slotTime: String,
-  coupon_Code: String,
-  amount: Number,
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  name: { type: String, required: true },
+  mobile: { type: String, required: true },
+  purpose: { type: String, required: true },
+  slotDate: { type: String, required: true },
+  slotTime: { type: String, required: true },
+  coupn_Code: { type: String },
+  amount: { type: Number, required: true },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
 });
+
+{
+  timestamps: true;
+}
+
 export default mongoose.model("Booking", bookingSchema);
