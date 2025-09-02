1228a6443fbae308d628dd0651110f70cc5b810d
Aditya Singh <83707197+Aditya-Singh714@users.noreply.github.com>
2025-09-02 22:23:54 +0530

    Update orderRoutes.js

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -2,11 +2,19 @@ import express from "express";
 import {
   createOrder,
   getOrders,
-  verifyPayment,
+  verifyPaymentRedirect,
 } from "../controllers/orderController.js";
 import authMiddleware from "../middlewares/authMiddleware.js";
+
 const router = express.Router();
+
+// Create Razorpay order
 router.post("/create-order", authMiddleware, createOrder);
+
+// Get all orders for logged-in user
 router.get("/", authMiddleware, getOrders);
-router.post("/verify-payment", authMiddleware, verifyPayment);
+
+// Verify payment with redirect flow
+router.post("/verify-payment-redirect", verifyPaymentRedirect);
+
 export default router;
